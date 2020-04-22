9d07754760ef791effb7a35ac9f4361564cae757
an4morph <avgustin456@gmail.com>
2020-04-22 17:38:35 +0600

    add details

--- fakebd.js
+++ fakebd.js
@@ -1,14 +1,27 @@
 module.exports = [
   {
     id: '1',
+    done: false,
     text: 'Read a book'
   },
   {
     id: '2',
+    done: true,
     text: 'Go to the market and buy vegetables'
   },
   {
     id: '3',
+    done: false,
     text: 'Meet with friends in ZOOM'
+  },
+  {
+    id: '4',
+    done: false,
+    text: 'Feed my cats'
+  },
+  {
+    id: '5',
+    done: true,
+    text: 'Call to mom'
   }
 ]
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 const allData = require('./fakebd')
 
+const colors = [null, '#efa8e4', '#97e5ef', '#f6d198', '#77d8d8', '#f2ed6f']
+
 app.get('/list', (req, res) => {
   res.send(allData)
 })
@@ -11,7 +13,7 @@ app.get('/list/:id', (req, res) => {
   const id = req.params.id
   const item = allData.find(i => i.id === id)
   if (!item) res.status(400).send('Bad Request')
-  res.send(item)
+  res.send({...item, description: `Description for task ${item.id}`, color: colors[item.id] })
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
