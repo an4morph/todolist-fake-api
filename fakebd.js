de2e89f0cdf65cf4e634d75ad9b7f9b6d95cb834
an4morph <avgustin456@gmail.com>
2020-04-22 15:25:14 +0600

    add get queries

---
+++ fakebd.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: '1',
+    text: 'Read a book'
+  },
+  {
+    id: '2',
+    text: 'Go to the market and buy vegetables'
+  },
+  {
+    id: '3',
+    text: 'Meet with friends in ZOOM'
+  }
+]
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const allData = require('./fakebd')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/list', (req, res) => {
+  res.send(allData)
+})
+
+app.get('/list/:id', (req, res) => {
+  const id = req.params.id
+  const item = allData.find(i => i.id === id)
+  if (!item) res.status(400).send('Bad Request')
+  res.send(item)
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
