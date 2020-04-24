154dd7b3f54637beae9229ed4241b41ab5d1cf14
an4morph <avgustin456@gmail.com>
2020-04-24 19:32:47 +0600

    add post, put and delete queries

--- defaultData.js
+++ defaultData.js
@@ -1,27 +1,18 @@
-module.exports = [
-  {
-    id: '1',
-    done: false,
-    text: 'Read a book'
-  },
-  {
-    id: '2',
-    done: true,
-    text: 'Go to the market and buy vegetables'
-  },
-  {
-    id: '3',
-    done: false,
-    text: 'Meet with friends in ZOOM'
-  },
-  {
-    id: '4',
-    done: false,
-    text: 'Feed my cats'
-  },
-  {
-    id: '5',
-    done: true,
-    text: 'Call to mom'
-  }
-]
+module.exports = {
+  "list": [
+    {
+      "id": "1",
+      "done": false,
+      "text": "Coding every day"
+    }
+  ],
+  "listExtended": [
+    {
+      "id": "1",
+      "done": false,
+      "text": "Coding every day",
+      "color": "#5cf",
+      "description": "Description for task 1"
+    }
+  ]
+}
--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const port = 3000
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const defaultData = require('./defaultData')
+const shortid = require('shortid')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-db.defaults({ list: defaultData }).write()
+db.defaults(defaultData).write()
 
 const colors = [null, '#efa8e4', '#97e5ef', '#f6d198', '#77d8d8', '#f2ed6f']
+
+const error = (res, status, text) => res.status(status).json(text).end()
+
+app.use(express.json()) 
 
 app.get('/list', (req, res) => {
   const list = db.get('list')
@@ -19,11 +24,44 @@ app.get('/list', (req, res) => {
 
 app.get('/list/:id', (req, res) => {
   const id = req.params.id
-  const item = db.get('list').find({ id })
+  const item = db.get('listExtended').find({ id })
   if (!item) res.status(400).send('Bad Request')
   res.send(item)
 })
 
-app.post
+app.post('/add', (req, res) => {
+  if (!req.body.text) return error(res, 400, 'text attribute is required')
+
+  const id = shortid.generate()
+  const { text, done } = req.body
+
+  db.get('list').push({ id, text, done }).write()
+  db.get('listExtended').push({ id, ...req.body }).write()
+  res.send({ id, ...req.body })
+})
+
+app.put('/edit/:id', (req, res) => {
+  const { id } = req.params
+  const item = db.get('list').find({ id })
+  const itemExtended = db.get('listExtended').find({ id })
+
+  if (!item.value()) error(res, 404, `Item with id (${id}) not found`)
+
+  item.assign(req.body).write()
+  itemExtended.assign(req.body).write()
+  res.send(itemExtended)
+})
+
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params
+  const item = db.get('list').find({ id })
+  const itemExtended = db.get('listExtended').find({ id })
+
+  if (!item.value()) error(res, 404, `Item with id (${id}) not found`)
+
+  item.remove({ id })
+  itemExtended.remove({ id })
+  res.send('success deleted')
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
