01e5809cd2a7a5d877288b67b5839219d0eb30ea
an4morph <avgustin456@gmail.com>
2020-04-24 15:57:02 +0600

    add lowdb database

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const allData = require('./fakebd')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const defaultData = require('./defaultData')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ list: defaultData }).write()
 
 const colors = [null, '#efa8e4', '#97e5ef', '#f6d198', '#77d8d8', '#f2ed6f']
 
 app.get('/list', (req, res) => {
-  res.send(allData)
+  const list = db.get('list')
+  res.send(list)
 })
 
 app.get('/list/:id', (req, res) => {
   const id = req.params.id
-  const item = allData.find(i => i.id === id)
+  const item = db.get('list').find({ id })
   if (!item) res.status(400).send('Bad Request')
-  res.send({...item, description: `Description for task ${item.id}`, color: colors[item.id] })
+  res.send(item)
 })
+
+app.post
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
